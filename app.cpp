60221cf7a9cffb5591b62f8b83568b276eb17251
H1577 <Haideralialmubarak1@gmail.com>
2026-01-12 01:39:16 +0300

    Massege

---
+++ app.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+//Include for including Libraries
+//Istream is a library for Functions
+// This is a Comment 
+int main()
+{
+ int price = 500; //This a variable 
+ std::cout << "The Price is " << price; // Before Adding The Varible Put <<  
+ std::cout << "\nThe new Price is "<< price * 9999999999;
+ std::cout << "\nThe Price for My Friend is " << price / 9999999999;
+
+ price = 300; //Update the Variable without int
+ std::cout << "\nThe Updated Price Is " << price + 500;
+ return 0;
+
+}
+/*
+By Me
+Date : 2026/1/11
+*/
