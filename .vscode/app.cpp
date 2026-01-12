45c23f81327885bffa2e4a62069f810acc797d2e
H1577 <Haideralialmubarak1@gmail.com>
2026-01-13 00:41:49 +0300

    .

--- .vscode/app.cpp
+++ .vscode/app.cpp
@@ -4,15 +4,18 @@
 // This is a Comment 
 int main()
 {
- int price = 500; //This a variable 
- std::cout << "The Price is " << price; // Before Adding The Varible Put <<  
+ int price = 500; 
+ //This a variable 
+ std::cout << "The Price is " << price; 
+ // Before Adding The Varible Put <<  
  std::cout << "\nThe new Price is "<< price * 9999999999;
+
  std::cout << "\nThe Price for My Friend is " << price / 9999999999;
 
- price = 300; //Update the Variable without int
+ price = 300; 
+ //Update the Variable without int
  std::cout << "\nThe Updated Price Is " << price + 500;
  return 0;
-
 }
 /*
 By Me
---
+++ First 10 Days/The First Day.cpp/2/Untitled-1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+int main()
+/*
+We can not add a number at the first letter of a vabriable 
+We can use _ in a vabiarable 
+In vabriales we use cameLcase . For example : myBestFreind
+*/
+{
+int k, l, j, h;
+ //we can use the vabriables before we give them value
+k = 50, l = 25, j = l;
+ // j = 25
+cout << j + l + k;
+//This is the vabriable before Update";
+cout << "\n" << k * j;
+//This is the multiplication of k and j => 25 * 50 
+cout << "\n" << "============================";
+
+k= 100; 
+// Updating the vabriable 
+cout << "\n" << j + l + k; 
+//This is the vabriable after Update"
+cout << "\n" << k * j; 
+//This is the multiplication of k and j after updating => 25 * 50
+cout << "\n" << "============================";
+return 0;
+}
+/*
+By me
+1/12
+*/
