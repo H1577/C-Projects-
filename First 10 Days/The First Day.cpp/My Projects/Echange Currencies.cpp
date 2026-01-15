4396ba30d964d900be35f0eb57da10890f022588
H1577 <Haideralialmubarak1@gmail.com>
2026-01-15 10:01:26 +0300

    Adding a new Project

---
+++ First 10 Days/The First Day.cpp/My Projects/Echange Currencies.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+//Currency exchange 
+//From USD to EUR 
+//From EUR to USD
+int main()
+{
+int _euro = 80;
+cout << "Value of Euro is " << _euro 
+<< " so it equals " << _euro * 1.16 << " USD";
+int _dollar = 93;
+cout << "\nValue of Dollar is " << _dollar 
+<< " so it equals " << _dollar * 0.86 << " EUR";
+return 0;
+}
+//2026/1/15
