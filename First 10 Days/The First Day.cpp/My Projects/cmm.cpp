f29a9def3a0e5ae4d608c6be792a78341662c3fc
H1577 <Haideralialmubarak1@gmail.com>
2026-01-23 15:01:32 +0300

    Learning

---
+++ First 10 Days/The First Day.cpp/My Projects/cmm.cpp
@@ -0,0 +1,14 @@
+#include  <iostream>
+using namespace std;
+int main()
+{
+cout << "line\n"; // \ for skipping a character and telling it is not a code
+cout << "line2\\\n"; // for printing a Back slash 
+cout << "line\t3\n"; // for taking four spaces
+cout << "line\b4\n"; // for delete the character before back slash 
+cout << "line \"5\"\n"; // to print ""
+cout << "li\rne\n"; // \r to delete li after that print ne 
+cout << true + false << "\n"; // True = 1 False = 0
+cout <<  99; //Numbers can be written without ""
+return 0; 
+}
