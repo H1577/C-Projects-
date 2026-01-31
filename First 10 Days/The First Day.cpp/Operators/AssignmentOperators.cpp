cbedc92d6decb4bdc722b819e46b6624eb6098bd
H1577 <Haideralialmubarak1@gmail.com>
2026-01-31 21:25:31 +0300

    .

---
+++ First 10 Days/The First Day.cpp/Operators/AssignmentOperators.cpp
@@ -0,0 +1,32 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int x = 99;
+  x += 1;              //x = x + 1
+  cout << x << "\n";
+
+  short y = 11;
+  y -= 1;              //y = y - 1
+  cout << y << "\n";
+
+  int z = 50;
+  z *= 50;             //z = z*50
+  cout << z << "\n";
+
+  int a = 'A';
+  a /= 5;              //a = a/5
+  cout << a << "\n";
+
+  int b = 'c';
+  b %= -true;             //b = b%1
+  cout << b << "\n";
+
+  return 0;
+}
+/*
+Date : 2026/1/31
+*/
+
