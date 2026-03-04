74b4325c37cc5900d8014c5862045aa73ab53365
H1577 <Haideralialmubarak1@gmail.com>
2026-03-05 00:13:21 +0300

    .

--- First 10 Days/The First Day.cpp/Solving Problems/DivisibilityProblem.cpp
+++ First 10 Days/The First Day.cpp/Solving Problems/DivisibilityProblem.cpp
@@ -5,27 +5,31 @@ int main(){
 
     int t;
     unsigned long long a,b;
-    int x = 0;
     int i = 0;
 
     cin >> t;
     int y[t];
     
     while(i < t){
+      int x = 0;
       cin >> a; cin >> b;
-    while(b != 0 && a%b != 0) 
-    {a=a+1; x++;}
-    y[i] = x; 
-    
+
+    if(a%b != 0){
+    x = b-(a%b);
+    y[i] = x;}
+    else   
+    y[i] = x;
+
     i++;
     }
 
     int z = 0;
-    while(b != 0 && z < t){
+    while(z < t){
         cout << y[z] << "\n";
         z++;
     }
     
     return 0;
 }
-
+//Solved :))
+//Date : 2026/3/4
